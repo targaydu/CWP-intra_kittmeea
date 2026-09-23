bbe177e608d86f5333e63dbe64321833e1c15fcf
targaydu <68050031@kmitl.ac.th>
2026-09-23 14:00:03 +0700

    asd

--- cell03/ex03/advance_mult.py
+++ cell03/ex03/advance_mult.py
@@ -1,1 +1,14 @@
-print
+import sys
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 0
+    while i <= 10:
+        print(f"Table de {i}: ", end="")
+        j = 0
+        while j <= 10:
+            print(f"{i * j} ", end="")
+            j += 1
+        print()
+        i += 1
+        
